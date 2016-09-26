a1ea1baeb72930c2ede7ecd47c0118ba482803a3
srscrls <srscrls@users.noreply.github.com>
2016-09-26 22:23:13 +0100

    Add files via upload

--- assignments/assignment2/assignment2-starter-code/app.js
+++ assignments/assignment2/assignment2-starter-code/app.js
@@ -1,13 +1,52 @@
 (function(){
 'use strict';
 	
-angular.module('ShoppingListCheckOff')
-.controller('ToBuyShoppingController',ToBuyShoppingController)
+angular.module('ShoppingListCheckOff',[])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
 .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
 .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService){
+	var buyItems = this;
+	buyItems.items = ShoppingListCheckOffService.getBuyItems();
+	buyItems.removeItem = function(itemIndex){
+		ShoppingListCheckOffService.buyItem(itemIndex);
+	}
+}
 
-
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService){
+	var boughtItems = this;
+	
+	boughtItems.items = ShoppingListCheckOffService.getBoughtItems();
+}
+	
+function ShoppingListCheckOffService() {
+	var service = this;
+	var buyItems = [
+		{name: "cookies", quantity: 10 },
+		{name: "chips", quantity: 10 },
+		{name: "chocolate", quantity: 10 },
+		{name: "biscuits", quantity: 10 },
+		{name: "fruit", quantity: 10 }
+	];
+	
+	var boughtItems = [];
+	
+	service.getBuyItems = function(){
+		return buyItems;
+	}
+	
+	service.getBoughtItems = function(){
+		return boughtItems;
+	}
+	
+	service.buyItem = function(itemIndex){
+		var item = buyItems.splice(itemIndex,1);
+		boughtItems.push({name: item[0].name, quantity: item[0].quantity});
+	}	
+}
 
 })();
 
