8af027440023b26f010c986253a040e2693b9984
Kostya <kostya>
2016-09-28 18:23:19 +0300

    remove unnecessary </div> tag and add two semicolons (;)

--- examples/Lecture19/app.js
+++ examples/Lecture19/app.js
@@ -25,10 +25,10 @@ function ChildController1($scope) {
   // console.log("$scope.parentValue: ", $scope.parentValue);
   // console.log($scope);
   //
-  // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue)
+  // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
   // $scope.pc.parentValue = 5;
   // console.log("** CHANGED: $scope.pc.parentValue = 5; ***");
-  // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue)
+  // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
   // console.log("$scope: ", $scope);
   //
   // console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
