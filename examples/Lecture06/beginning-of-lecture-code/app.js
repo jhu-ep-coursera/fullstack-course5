2aeaa0fee14773b8ffaf4774fc416cc2cebbf531
Jennifer Jiang2 <bingqian.jiang@factset.com>
2017-08-17 11:12:49 -0400

    Finish Week1 examples

--- examples/Lecture06/beginning-of-lecture-code/app.js
+++ examples/Lecture06/beginning-of-lecture-code/app.js
@@ -1,7 +1,39 @@
-(function () {
+/*(function () {
 'use strict';
 
+angular.module('nameCalculator', []) 
 
+.controller('calController', function($scope) {
+	$scope.name = '';
+	$scope.calculate = function() {
+		var total = 0;
+		for (var i = 0; i < $scope.name.length; i++) {
+			total += $scope.name.charCodeAt(i);
+		}
+		// for (var i = 0; i < name.length; i++) {
+		// 	total += name.charCodeAt(i);
+		// }
+		return total;
+	}
 
+});
+})();
+*/
 
+(function() {
+	'use strict';
+
+	angular.module('nameCalculator', []) 
+
+	.controller('calController', function($scope) {
+		$scope.name = '';
+		$scope.calculate = function() {
+			var total = 0;
+			for (var i = 0; i < $scope.name.length; i++) {
+				total += $scope.name.charCodeAt(i);
+			}
+			console.log($scope);
+			return total;
+		}
+	});
 })();
--- examples/Lecture09/beginning-of-lecture-code/app.js
+++ examples/Lecture09/beginning-of-lecture-code/app.js
@@ -4,8 +4,19 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
-function DIController ($scope) {
-  $scope.name = "Yaakov";
+function DIController ($scope, $filter, $injector) {
+  $scope.name = "";
+  $scope.upper = function() {
+  	$scope.name = $filter('uppercase')($scope.name);
+  }
+
+  function AnnonateMe(name, job, blah) {
+  	return 'Blah!';
+  }
+
+  console.log(AnnonateMe);
+  console.log($injector.annotate(DIController));
 }
+
 
 })();
