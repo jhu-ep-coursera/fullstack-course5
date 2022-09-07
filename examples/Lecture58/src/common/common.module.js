0495a526ba8ebb5d3c8d1e2fe2a69d9191ad7fa2
Yaakov Chaikin <yaakov@ClearlyDecoded.com>
2022-09-06 23:54:35 -0400

    ApiPath constant updated everywhere

--- examples/Lecture58/src/common/common.module.js
+++ examples/Lecture58/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://coursera-jhu-default-rtdb.firebaseio.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
